c6a88baac3277974deb382ba7c1964e2f63a2fbc
Just Strato <Just Strato@Just-Strato>
2018-04-13 11:58:07 +0200

    V2

--- Hearthstone/src/Carte.java
+++ Hearthstone/src/Carte.java
@@ -5,22 +5,36 @@ import heartstone.src.heartstone.IJoueur
  * @author JustStrato&Aazarias
  *
  */
-public abstract class Carte implements ICarte{
+public abstract class Carte implements ICarte {
 	private String nom;
 	private int cout;
 	private IJoueur proprietaire;
+	private ICapacite capacite;
 	
-	public Carte(String nom, int cout, IJoueur proprietaire) {
-		this.nom=nom;
-		this.cout=cout;
+	public Carte(String nom, int cout, IJoueur proprietaire, ICapacite capacite) {
+		this.setNom=nom;
+		this.setCout=cout;
 		this.proprietaire=proprietaire;
+		this.capacite=capacite;
 	}
 	
 	//gettersetters
+	private void setNom(String nom) {
+		if(this.nom!=null && this.nom!="") {
+			this.nom=nom;
+		}
+		else
+			throw new IllegalArgException("Nom vide");
+	}
 	public String getNom() {
 		return this.nom;
 	}
-	
+	private void setCout(int cout) {
+		if(cout<0 || cout>10)
+			throw new IllegalArgException("Nom vide");
+		else
+			this.cout=cout;
+	}
 	public int getCout() {
 		return this.cout;
 	}
