327405536e9ca599dc471ad043e4d626b5b070c2
Just-Strato <38212460+Just-Strato@users.noreply.github.com>
2018-05-20 14:38:04 +0200

    Classe Abstraite Carte

---
+++ Hearthstone/src/carte/carte.java
@@ -0,0 +1,40 @@
+package Hearthstone.src.carte;
+
+/**
+*
+*@author Badr Matthieu
+*/
+public abstract class Carte implements ICarte {
+	private String nom;
+	private int cout;
+	private IJoueur proprietaire;
+	private ICapacite capacite;
+	
+	public Carte(String nom, int cout, IJoueur proprietaire, ICapacite capacite) {
+		this.nom = nom;
+		this.cout = cout;
+		this.proprietaire=proprietaire;
+		this.capacite=capacite;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public int getCout(){
+		return this.cout;
+	}
+	
+	public ICapacite getCapacite() {
+		return this.capacite;
+	}
+	
+	public IJoueur getProprietaire() {
+		return this.proprietaire;
+	}
+	
+	public String toString(){
+		return this.nom+" coûte "this.cout + "mana";
+	}
+	
+}
