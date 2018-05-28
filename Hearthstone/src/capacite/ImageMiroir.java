95af012b7047ffb3b4580c341a6eebd2d0feb295
Just Strato <Just Strato@Just-Strato>
2018-05-28 18:28:19 +0200

    Fixage d'erreur

--- Hearthstone/src/capacite/ImageMiroir.java
+++ Hearthstone/src/capacite/ImageMiroir.java
@@ -1,14 +1,17 @@
 package capacite;
 
+import carte.Serviteur;
 import jeu.HearthstoneException;
+import jeu.IJoueur;
 
 /**
 *
 *@author Badr Matthieu
 */
 public class ImageMiroir extends InvocationServiteur {
-	public ImageMiroir() {
-		super("Image Miroir", "Invoque 2 serviteurs 0/2 avec la capacite Provocation", new Serviteur("Image Remanente", 1, Plateau.getPlateau().getJoueurCourant(), new Provocation(), 0, 2), 2);
+	public ImageMiroir(IJoueur joueur) throws HearthstoneException {
+		super("Image Miroir", "Invoque 2 serviteurs 0/2 avec la capacite Provocation", 
+				new Serviteur("Image Remanente", 1, joueur, new Provocation(), 0, 2), 2);
 	}
 	
 	public void executerEffetMiseEnJeu(Object cible) throws HearthstoneException {
